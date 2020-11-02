040bac70486af6cec9127f1660958b514d52e705
Grigore Stefan <g_stefan@yahoo.com>
2020-11-02 02:30:46 +0200

    update

--- build.config.cmd
+++ build.config.cmd
@@ -3,4 +3,6 @@ rem Public domain
 rem http://unlicense.org/
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
+set PRODUCT_NAME=installer-netbeans
 set PRODUCT_VERSION=12.0
+set PRODUCT_BASE=netbeans
--- build.installer.cmd
+++ build.installer.cmd
@@ -3,9 +3,9 @@ rem Public domain
 rem http://unlicense.org/
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
-echo -^> installer netbeans
-
 call build.config.cmd
+
+echo -^> installer %PRODUCT_NAME%
 
 if exist installer\ rmdir /Q /S installer
 mkdir installer
@@ -15,6 +15,6 @@ mkdir build
 
 makensis.exe /NOCD "util\netbeans-installer.nsi"
 
-call grigore-stefan.sign "NetBeans" "installer\netbeans-%PRODUCT_VERSION%-installer.exe"
+call grigore-stefan.sign "NetBeans" "installer\%PRODUCT_BASE%-%PRODUCT_VERSION%-installer.exe"
 
 if exist build\ rmdir /Q /S build
