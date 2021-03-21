744791a7dff000bfc837f13f9abe8317eb346214
Grigore Stefan <g_stefan@yahoo.com>
2021-03-21 23:42:30 +0200

    update

--- build.config.cmd
+++ build.config.cmd
@@ -4,5 +4,5 @@ rem http://unlicense.org/
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
 set PRODUCT_NAME=installer-netbeans
-set PRODUCT_VERSION=12.2
+set PRODUCT_VERSION=12.3
 set PRODUCT_BASE=netbeans
