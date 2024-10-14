6b33eaebf4174a1399a41380dcde4ae481a47305
Grigore Stefan <g_stefan@yahoo.com>
2024-10-14 16:57:03 +0300

    Update

--- fabricare/vendor.js
+++ fabricare/vendor.js
@@ -9,7 +9,7 @@ Shell.mkdirRecursivelyIfNotExists("vendor");
 
 var vendor = "netbeans-" + Project.version + "-bin.zip";
 if (!Shell.fileExists("vendor/" + vendor)) {
-	var webLink = "https://mirrors.hostingromania.ro/apache.org/netbeans/netbeans/" + Project.version + "/" + vendor;
+	var webLink = "https://dlcdn.apache.org/netbeans/netbeans/" + Project.version + "/" + vendor;
 	var cmd = "curl --insecure --location " + webLink + " --output vendor/" + vendor;
 	Console.writeLn(cmd);
 	exitIf(Shell.system(cmd));
